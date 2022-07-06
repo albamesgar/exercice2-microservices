54592dbd3694c176a5120fd79333068b90c12263
Alba Mesa <alba.mesa.garrido@gmail.com>
2022-07-06 09:47:51 +0200

    Price repository

--- price-service/src/main/java/com/ironhack/priceservice/repository/PriceRepository.java
+++ price-service/src/main/java/com/ironhack/priceservice/repository/PriceRepository.java
@@ -3,7 +3,9 @@ package com.ironhack.priceservice.repository;
 import com.ironhack.priceservice.controller.dto.PriceDTO;
 import com.ironhack.priceservice.model.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PriceRepository extends JpaRepository<Price,Long> {
 
 }
